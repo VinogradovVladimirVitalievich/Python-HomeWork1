555164e9b7cfb4167a094408decce03173decee1
Vladimir <Vinogradov.v@tseh85.ru>
2022-10-11 20:56:44 +0300

    задача 1

--- Seminar002.py
+++ Seminar002.py
@@ -1,12 +1,28 @@
 # Задача 0. Дано число N. найти все енр делители.
 # Для каждого делителя укажите чётный он или не чётный.
 
-def zadacha0():
-    number = 60
-    for i in range(1, number + 1):
-       if number % i == 0:
-           print(i, end = " ")
-           if i % 2 == 0:
-            print(" - чётный")
-           else:
-            print(" - нечётный")   
+
+# def CheckEven(x):
+#     if x % 2 == 0:
+#             return("- чётный")
+#     else:
+#             return("- нечётный")  
+
+# def Zadacha0():         
+#  number = 60
+#  for i in range(1, number + 1):
+#        if number % i == 0:
+#            print(i, end=" ")
+#            print(CheckEven(i))  
+# Zadacha0()
+
+
+# Задача 1. Выведите таблицу истинности для выражения ¬ X ∨ Y.
+
+# print("x | y | ¬ X ∨ Y")
+# for x in range(0,2):
+#     for y in range(0,2):
+#         sum = not x or y
+#         print(f"{x} | {y} | {int(sum)}")
+        
+
