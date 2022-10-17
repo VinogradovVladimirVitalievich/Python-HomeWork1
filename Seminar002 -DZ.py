f4bda2f9de39bfa228aa420604394b738fb71487
Vladimir <Vinogradov.v@tseh85.ru>
2022-10-17 13:14:55 +0300

    четвёртая  задача

--- Seminar002 -DZ.py
+++ Seminar002 -DZ.py
@@ -23,27 +23,32 @@
 # Задача 3. Даны две строки. Посчитайте сколько раз каждый символ первой строки встречается во второй
 # «one» «onetwonine» - o – 2, n – 3, e – 2
 
-def LikeSymbols(str1, str2):
-    len1 = len(str1)
-    len2 = len(str2)
-    list = []
-    str3 = None
-    for i in range(len1):
-        count = 0
-        for j in range(len2):
-            if str1[i] == str2[j]:
-                count += 1
-        str3 = f"{str1[i]} - {count}"
-        list.append(str3)
-    return list
-str1 = 'one'
-str2 = 'onetwonine'
-print(LikeSymbols(str1, str2))
+# def LikeSymbols(str1, str2):
+#     len1 = len(str1)
+#     len2 = len(str2)
+#     list = []
+#     str3 = None
+#     for i in range(len1):
+#         count = 0
+#         for j in range(len2):
+#             if str1[i] == str2[j]:
+#                 count += 1
+#         str3 = f"{str1[i]} - {count}"
+#         list.append(str3)
+#     return list
+# str1 = "one"
+# str2 = "onetwonine"
+# print(LikeSymbols(str1, str2))
 
 
     
 
-
-
 # Задача 4. Задайте список из N элементов, заполненных числами из промежутка [-N, N]. Сдвиньте все элементы списка на 2 позиции вправо.
 # 3 -> [2, 3, -3, -2, -1, 0, 1]
+
+
+# lst = [2, 3, -3, -2, -1, 0, 1]
+# e1 = lst[-2]
+# for i, e2 in enumerate(lst):
+#     lst[i], e1 = e1, e2
+# print(lst)
