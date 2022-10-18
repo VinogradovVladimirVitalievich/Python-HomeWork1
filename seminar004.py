d2e59b5caddf389be9a0b3435c79724f3381ee11
Vladimir <Vinogradov.v@tseh85.ru>
2022-10-18 21:10:58 +0300

    1 задача

--- seminar004.py
+++ seminar004.py
@@ -21,18 +21,15 @@
 
 # Задача 1. Создайте кортеж, заполненный случайными числами. Напишите метод, который заменяет элемент в кортеже по заданному индексу. 
 # переменная tuple - обозначение кортежа
-import random
 
-def chenge_index(numbers, index):
-    numbers = list(numbers)
-    numbers[index - 1] = random.randint(10, 100)
-    numbers = tuple(numbers)
-    return(numbers)
-
-size = random.randint(5, 12)
-numbers = tuple(random.randint(10, 100) for i in range(10))
-print(numbers)
-
-index = int(input("Введите индекс, который необходимо заменить: "))
-
-print(chenge_index(numbers, index))
+# import random
+# def chenge_index(numbers, index):
+#     numbers = list(numbers)
+#     numbers[index - 1] = random.randint(10, 100)
+#     numbers = tuple(numbers)
+#     return(numbers)
+# size = random.randint(5, 12)
+# numbers = tuple(random.randint(10, 100) for i in range(10))
+# print(numbers)
+# index = int(input("Введите индекс, который необходимо заменить: "))
+# print(chenge_index(numbers, index))
