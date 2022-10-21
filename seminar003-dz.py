9249f06d8c468686bbf92bf9763cdabd1e534028
Vladimir <Vinogradov.v@tseh85.ru>
2022-10-21 14:39:23 +0300

    3 задача

--- seminar003-dz.py
+++ seminar003-dz.py
@@ -23,24 +23,27 @@
 #         if i[0].lower() == letter.lower():
 #             print(i)
 
+
+
+
 # Задача 3. Создайте скрипт бота, который находит ответы на фразы по ключу в словаре. Бот должен, как минимум, отвечать на фразы «привет», «как тебя зовут». Если фраза ему неизвестна, он выводит соответствующую фразу.
 # «как тебя зовут?» –> меня зовут Анатолий
 
-from multiprocessing.resource_sharer import stop
 
+# from multiprocessing.resource_sharer import stop
 
-dictionary = \
-    {
-        "Привет" : "Салют!", "Как тебя зовут?" : "Меня зовут Владимир", "Выход": "Пока!"
-    }
-isStarted = True
-while isStarted:
-    answer = input("Я: ")
-    if answer == " выход":
-        isStarted = not isStarted
-    if answer in dictionary.keys():
-        print("Бот:", dictionary[answer])
-    else:
-        print("Бот: неизвестная команда, попробуйте снова")
-        break
-stop
+# dictionary = \
+#     {
+#         "Привет" : "Салют!", "Как тебя зовут?" : "Меня зовут Владимир", "Выход": "Пока!"
+#     }
+# isStarted = True
+# while isStarted:
+#     answer = input("Я: ")
+#     if answer == " выход":
+#         isStarted = not isStarted
+#     if answer in dictionary.keys():
+#         print("Бот:", dictionary[answer])
+#     else:
+#         print("Бот: неизвестная команда, попробуйте снова")
+#         break
+# stop
