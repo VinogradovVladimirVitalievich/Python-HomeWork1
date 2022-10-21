0b86beeb3d3deb78cafa2b9fd835a51fc7617a0b
Vladimir <Vinogradov.v@tseh85.ru>
2022-10-21 14:26:57 +0300

    2 задача

--- seminar003-dz.py
+++ seminar003-dz.py
@@ -2,18 +2,26 @@
 # 6 –> 1 1 2 3 5 8
 
 
-firstnumber = 0
-secondnumber = 1
-count = 1000
-data = open("fibonachi.txt", "w")
-for i in range(count):
-    data.writelines(str(firstnumber)+ "\n")
-    (firstnumber, secondnumber) = (secondnumber, firstnumber + secondnumber)
+# firstnumber = 0
+# secondnumber = 1
+# count = 1000
+# data = open("fibonachi.txt", "w")
+# for i in range(count):
+#     data.writelines(str(firstnumber)+ "\n")
+#     (firstnumber, secondnumber) = (secondnumber, firstnumber + secondnumber)
 
 
 
 # Задача 2. В файле находятся названия фруктов. Выведите все фрукты, названия которых начинаются на заданную букву.
 # а –> абрикос, авокадо, апельсин, айва.
+
+with open("Fruits.txt", encoding = "utf-8") as data:
+    Fruits = data.readline().split()    
+    print(Fruits)
+    letter = "а"
+    for i in Fruits:
+        if i[0].lower() == letter.lower():
+            print(i)
 
 # Задача 3. Создайте скрипт бота, который находит ответы на фразы по ключу в словаре. Бот должен, как минимум, отвечать на фразы «привет», «как тебя зовут». Если фраза ему неизвестна, он выводит соответствующую фразу.
 # «как тебя зовут?» –> меня зовут Анатолий
