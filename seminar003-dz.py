d11ec944c51e4482a8731642cb02a813bd52f9e0
Vladimir <Vinogradov.v@tseh85.ru>
2022-10-21 14:39:05 +0300

    3 задача

--- seminar003-dz.py
+++ seminar003-dz.py
@@ -15,13 +15,32 @@
 # Задача 2. В файле находятся названия фруктов. Выведите все фрукты, названия которых начинаются на заданную букву.
 # а –> абрикос, авокадо, апельсин, айва.
 
-with open("Fruits.txt", encoding = "utf-8") as data:
-    Fruits = data.readline().split()    
-    print(Fruits)
-    letter = "а"
-    for i in Fruits:
-        if i[0].lower() == letter.lower():
-            print(i)
+# with open("Fruits.txt", encoding = "utf-8") as data:
+#     Fruits = data.readline().split()    
+#     print(Fruits)
+#     letter = "а"
+#     for i in Fruits:
+#         if i[0].lower() == letter.lower():
+#             print(i)
 
 # Задача 3. Создайте скрипт бота, который находит ответы на фразы по ключу в словаре. Бот должен, как минимум, отвечать на фразы «привет», «как тебя зовут». Если фраза ему неизвестна, он выводит соответствующую фразу.
 # «как тебя зовут?» –> меня зовут Анатолий
+
+from multiprocessing.resource_sharer import stop
+
+
+dictionary = \
+    {
+        "Привет" : "Салют!", "Как тебя зовут?" : "Меня зовут Владимир", "Выход": "Пока!"
+    }
+isStarted = True
+while isStarted:
+    answer = input("Я: ")
+    if answer == " выход":
+        isStarted = not isStarted
+    if answer in dictionary.keys():
+        print("Бот:", dictionary[answer])
+    else:
+        print("Бот: неизвестная команда, попробуйте снова")
+        break
+stop
