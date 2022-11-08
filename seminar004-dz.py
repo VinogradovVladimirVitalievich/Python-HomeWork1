2e139d37c13fc0953aa867b19ee75a1381b28cc9
Vladimir <deadman1@bk.ru>
2022-11-08 19:50:48 +0300

    задачи

--- seminar004-dz.py
+++ seminar004-dz.py
@@ -49,8 +49,48 @@
 # 2. «Сливочное», «Вафелька», «Сладкоежка»
 # Закончилось: «Бурёнка»
 
+# with open('icecream.txt', 'r', encoding='utf-8') as inf:
+#     assortment = set(inf.readline().rstrip().split(', '))
+#     current = set(inf.readline().rstrip().split(', '))
+#     print(f'Закончилось:', *assortment - current)
+
 # Задача 3. Выведите число π с заданной точностью. Точность вводится пользователем в виде натурального числа.3 -> 3.142
 # 5 -> 3.14159
 
-# Задача 4*. Даны два файла, в каждом из которых находится запись многочлена. Найдите сумму данных многочленов.
-# 1. 5x^2 + 3x2. 3x^2 + x + 8Результат: 8x^2 + 4x + 8
+# from math import pi as PI
+# from random import uniform as ru
+
+
+# def get_int_number(*args): return int(input(*args))
+
+
+# def print_pi(pi, dg):
+#     print(round(pi, dg))
+
+
+# def finding_pi():
+#     n = 10 ** 7  # количество испытаний
+#     k = 0  # количество точек внутри фигуры
+#     s0 = 4  # площадь квадрата в котором фигура
+#     print('waiting for counting PI')
+#     for _ in range(n):
+#         x = ru(-1, 1)
+#         y = ru(-1, 1)
+
+#         if x ** 2 + y ** 2 <= 1:
+#             k += 1
+#     print('DONE')
+#     return (k / n) * s0
+
+
+# def main():
+#     n = get_int_number('how many digits in PI: ')
+#     var = get_int_number('1 to math.PI, else to Monte Carlo finding PI: ')
+#     if var == 1:
+#         print_pi(PI, n)
+#     else:
+#         print_pi(finding_pi(), n)
+
+
+# if __name__ == '__main__':
+#     main()
